d629e643e6bdab763a9fe9a03e4427a59c9f60ac
TheShamRio <theshamrio@gmail.com>
2025-03-15 23:38:07 +0300

    Изменен заголовок CORS для поддержки нового порта; обновлены стили и логика в компоненте регистрации пользователя.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -41,7 +41,7 @@ def create_app():
     def handle_preflight():
         if request.method == 'OPTIONS':
             response = make_response()
-            response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3001'
+            response.headers['Access-Control-Allow-Origin'] = "http://localhost:3000"
             response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
             response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-CSRFToken'
             response.headers['Access-Control-Allow-Credentials'] = 'true'
