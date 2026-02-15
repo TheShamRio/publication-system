7dd5a556675da85650c2f847f337e74b5887c489
vitaly <hahabuntun@gmail.com>
2026-02-15 20:29:34 +0300

    setup: add repositories, services and enums files

---
+++ backend/repositories/plan_repository.py
@@ -0,0 +1,18 @@
+# get all plans
+
+# get plan by id
+
+# add plan
+
+# edit plan
+
+# get history of plan status changes
+
+# remove plan
+
+# add item to plan
+
+# edit plan item
+
+# remove plan item
+
