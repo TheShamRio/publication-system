f47303621b1fb6e7d16f7679f94119ea4236cb4a
vitaly <hahabuntun@gmail.com>
2026-02-16 22:00:57 +0300

    remove unneccessary files

--- backend_old/app/utils.py
+++
@@ -1,5 +0,0 @@
-from werkzeug.utils import secure_filename
-import os
-
-def allowed_file(filename, allowed_extensions=['pdf', 'docx', 'bib']):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
